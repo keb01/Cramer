119f0e82229c5b5d010f06b96c5fe873a31f6f1f
teren <teren@172.16.0.4>
2018-01-15 10:48:27 +0100

    DATABASE Test

---
+++ Cramer/src/pdstest/Test.java
@@ -0,0 +1,104 @@
+package pdstest;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Test {
+
+	public static void main(String[] args) throws SQLException {
+		select();
+		insert();
+		select();
+		/*update();
+		select();
+		delete();	
+		select();*/
+	}
+
+	public static void insert() throws SQLException {
+		
+		Connection cn =null;
+		
+		try {			
+			Statement st =null;
+			cn = Database.getConnection();
+			st = cn.createStatement();
+
+			String sql = "INSERT INTO Client (nom,prenom,adresse,email,tel,sexe,caracteristiques) VALUES ('Bismuth','Keren','','kerenbismuth@gmail.com','0601340606','Femme','')";
+
+			st.executeUpdate(sql);
+
+			} catch (SQLException e) {
+					e.printStackTrace();
+			} finally {
+			cn.close();	
+			}
+		}
+	
+	public static void update() throws SQLException {
+		
+		Connection cn =null;
+		
+		try {			
+			Statement st =null;
+			cn = Database.getConnection();
+			st = cn.createStatement();
+
+			String sql = "Update Client set nom = 'femme' where id_client = 1";
+
+			st.executeUpdate(sql);
+
+			} catch (SQLException e) {
+					e.printStackTrace();
+			} finally {
+			cn.close();	
+			}
+		}
+	
+	public static void delete() throws SQLException {
+		Connection cn =null;
+		
+		try {			
+			Statement st =null;
+			cn = Database.getConnection();
+			st = cn.createStatement();
+
+			String sql = "Delete from Client where id_client = 1";
+
+			st.executeUpdate(sql);
+
+			} catch (SQLException e) {
+					e.printStackTrace();
+			} finally {
+			cn.close();	
+			}
+		}
+	public static void select() throws SQLException {
+		Connection cn =null;
+		
+		
+		try {
+			
+			Statement st =null;
+			ResultSet rs =null;
+			cn = Database.getConnection();
+			
+			st = cn.createStatement();
+			String sql = "SELECT * FROM Client";
+			rs = st.executeQuery(sql);
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("id_client")+"|"+rs.getString("Nom")+"|"+rs.getString("Prenom"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();	
+		}finally{
+			
+			cn.close();
+			
+		}
+	}
+	
+}
+
