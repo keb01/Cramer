c5ee1fa86e31047ea53e35440b23961ad087c24a
teren <teren@10.10.6.154>
2018-01-15 16:00:43 +0100

    RE

---
+++ Cramer/src/pdstest/Client.java
@@ -0,0 +1,77 @@
+package pdstest;
+
+public class Client {
+
+	
+	String nom,prenom,adresse,email,tel,sexe,caracteristiques;
+
+	
+	
+	public Client(String nom, String prenom, String adresse, String email, String tel, String sexe,
+			String caracteristiques) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		this.email = email;
+		this.tel = tel;
+		this.sexe = sexe;
+		this.caracteristiques = caracteristiques;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getSexe() {
+		return sexe;
+	}
+
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+
+	public String getCaracteristiques() {
+		return caracteristiques;
+	}
+
+	public void setCaracteristiques(String caracteristiques) {
+		this.caracteristiques = caracteristiques;
+	}
+	
+}
---
+++ Cramer/src/pdstest/Database.java
@@ -0,0 +1,41 @@
+package pdstest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Database {
+
+		 private static final String URL = "jdbc:mysql://picpicb.ddns.net:5000/devCramer";
+		 private static final String USER = "bddcramer";
+		 private static final String PASSWORD = "cramer";
+		 private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+		 
+		 static {
+			 try {
+				 Class.forName(DRIVER_NAME).newInstance();
+				 System.out.println("* Driver loaded.");
+			 }
+			 catch (ClassNotFoundException e) {
+				 System.err.println("* ERROR: Driver " + DRIVER_NAME + "not found");
+			 }
+			 catch (InstantiationException e) {
+				 System.err.println("* ERROR: Impossible to create aninstance of " + DRIVER_NAME);
+				 System.err.println(e.getMessage());
+			 }
+			 catch (IllegalAccessException e) {
+				 System.err.println("* ERROR: Impossible to create aninstance of " + DRIVER_NAME);
+				 System.err.println(e.getMessage());
+			 }
+		 }
+		 
+		public static Connection getConnection() throws SQLException {
+			 return DriverManager.getConnection(URL, USER, PASSWORD);
+		}
+		 
+	}
+
+
