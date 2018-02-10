a91d337cdb51f6c431987067d2ef7fce8fc81efc
ASUS <ASUS@10.10.20.137>
2018-02-10 16:45:47 +0100

    <ajout de panelliste>

---
+++ Cramer/src/Prototype/PanelListe.java
@@ -0,0 +1,19 @@
+package Prototype;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+
+public class PanelListe extends JPanel{
+	public PanelListe(){
+
+		this.setBackground(Color.YELLOW);
+		this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		
+		
+	}
+
+	
+}
