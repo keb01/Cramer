60dc08596cfa3b847fde741dc66c705434102f28
teren <teren@LAPTOP-S7MFGA7D>
2018-04-14 14:41:31 +0200

    <CLIENT>

---
+++ src/Model/Client.java
@@ -0,0 +1,35 @@
+package Model;
+
+public class Client {
+
+	private long id,idPersonne;
+
+	public Client() {
+		
+	}
+
+	public Client(long id, long idPersonne) {
+		this.id = id;
+		this.idPersonne = idPersonne;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getIdPersonne() {
+		return idPersonne;
+	}
+
+	public void setIdPersonne(long idPersonne) {
+		this.idPersonne = idPersonne;
+	}
+	
+	
+	
+	
+}
